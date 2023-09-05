7416b4b7e0d614d9d590f811692e269e14a3e782
THEChrismoth <rabotdanklev@gmail.com>
2023-09-05 20:41:34 +0300

    Code review
    1)изменил ввод с вирутальной клавиатуры
    2)добавил функцию отправки изображений
    3)оптимизировал работу сообщений с AI

--- main.py
+++ main.py
@@ -1,143 +1,142 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import openai
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import random
 import requests
-from io import BytesIO
-import matplotlib.pyplot as plt
-from config import vk_group_id, vk_group_token, openai_api_key
 
-vk_group_id = vk_group_id
-vk_group_token = vk_group_token
-openai.api_key = openai_api_key
+# Ваш Access Token
+VK_TOKEN= "vk1.a.LWZ4oNx1PFVmjGqWEal83WHLEioPVUppDY0zNDqR23HBVqFqhHtiKpI78HWn-d-EahZsXhGacqc5IUnJSGDbVXLFm5Dtjnz1L-cT54XT-d_qqUMjUCK7uJClVHE3D6VAwindWj7ot-0ZsHfEo8e0ejacKVbRAH7k3WdKOVpCCnqY-kTddDGr_rAv7gTQNiPux2ACk5SS64jWrSPnF2dZ0A"
+# Ваш API-ключ ChatGPT
+CHATGPT_API_KEY = "your_chatgpt_api_key"
+# Ваш API-ключ Midjourney
+MIDJOURNEY_API_KEY = "your_midjourney_api_key"
 
-# Функция отправки сообщения
-def send_message(user_id, message, attachments=None):
-    if attachments:
-        vk.messages.send(
-            user_id=user_id,
-            random_id=vk_api.utils.get_random_id(),
-            message=message,
-            attachment=attachments
-        )
-    else:
-        vk.messages.send(
-            user_id=user_id,
-            random_id=vk_api.utils.get_random_id(),
-            message=message
-        )
 
-# Функция создания клавиатуры
-def create_keyboard():
-    keyboard = {
-        "one_time": False,
-        "buttons": [
-            [{
-                "action": {
-                    "type": "text",
-                    "label": "GPT"
-                },
-                "color": "positive"
-            }],
-            [{
-                "action": {
-                    "type": "text",
-                    "label": "Midjourney"
-                },
-                "color": "positive"
-            }]
-        ]
-    }
-    return keyboard
-
-# Функция генерации изображения Midjourney
-def generate_midjourney_image(description):
-    request_url = "https://api.eleuther.ai/midjourney/vqgan-clip-top-k-sample"
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "prompts": [description],
-        "truncation": 0.9,
-        "temperature": 0.9,
-        "top_k": 0
-    }
-
-    response = requests.post(request_url, json=data, headers=headers)
-    response_data = response.json()
-    image_url = response_data["image"]
-
-    return image_url
-
-# Функция генерации текстового ответа CHATGPT
-def generate_chatgpt_response(prompt):
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        temperature=0.6,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        timeout=5
-    )
-
-    return response.choices[0].text.strip()
-
-# Функция обработки сообщений
-def handle_message(event):
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        user_id = event.user_id
-        message = event.text
-
-        # Проверяем, является ли пользователь подписчиком сообщества
-        is_subscriber = vk.groups.isMember(group_id=vk_group_id, user_id=user_id)
-
-        if not is_subscriber:
-            send_message(user_id, "Извините, вы должны быть подписчиком сообщества, чтобы использовать бота.")
-            return
-
-        if message.lower() == 'gpt':
-            # Отправляем клавиатуру с кнопками GPT
-            keyboard = create_keyboard()
-            send_message(user_id, "Выберите функционал:", keyboard)
-    
-        elif message.lower() == 'midjourney':
-            # Отправляем клавиатуру с кнопками Midjourney
-            keyboard = create_keyboard()
-            send_message(user_id, "Выберите функционал:", keyboard)
-            
-        elif message.lower() == 'привет':
-            send_message(user_id, "Привет! Я полезный ассистент. Отправьте команду 'GPT' для работы с GPT или 'Midjourney' для работы с Midjourney.")
-
-        elif message.lower().startswith('midjourney_generate_image:'):
-            # Извлекаем описание изображения из сообщения
-            description = message.split(':', 1)[1].strip()
-            
-            # Генерируем изображение Midjourney
-            image_url = generate_midjourney_image(description)
-            
-            # Отправляем изображение пользователю
-            response = requests.get(image_url)
-            img_data = response.content
-            img = BytesIO(img_data)
-            plt.imshow(plt.imread(img))
-            plt.axis('off')
-            plt.show()
-            
-            send_message(user_id, "Ваше изображение Midjourney:")
-            send_message(user_id, "", attachments=img)
-            
-        else:
-            # Отправляем сообщение в CHATGPT API и получаем ответ
-            response = generate_chatgpt_response(message)
-            
-            send_message(user_id, response)
-
-# Инициализация сессии VK API
-vk_session = vk_api.VkApi(token=vk_group_token)
+# Подключение к API ВКонтакте
+vk_session = vk_api.VkApi(token=VK_TOKEN)
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
-# Основной цикл обработки событий
+# Список подписчиков рассылки
+subscribers = []
+
+# Функция для отправки запроса к ChatGPT
+def chat_gpt_request(message):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {CHATGPT_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "prompt": message,
+        "max_tokens": 50
+    }
+    response = requests.post(url, json=data, headers=headers)
+    return response.json()["choices"][0]["text"]
+
+# Функция для отправки запроса к Midjourney
+def midjourney_request(message):
+    url = "https://api.midjourney.com/conversation"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {MIDJOURNEY_API_KEY}"
+    }
+    data = {
+        "messages": [message]
+    }
+    response = requests.post(url, json=data, headers=headers)
+    return response.json()["response"]
+
+# Функция для отправки рассылки
+def send_newsletter(message):
+    for subscriber in subscribers:
+        vk.messages.send(user_id=subscriber, message=message, random_id=random.getrandbits(31))
+
+# Создание клавиатуры
+def create_keyboard():
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button("Подписаться на рассылку", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Отписаться от рооссылки", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button("ChatGPT", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Midjourney", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button("Написать админу", color=VkKeyboardColor.POSITIVE)
+    return keyboard.get_keyboard()
+
+# Функция для обработки рассылки
+def handle_newsletter(message):
+    if message.startswith("рассылка:"):
+        newsletter_text = message.replace("рассылка:", "").strip()
+        send_newsletter(newsletter_text)
+        vk.messages.send(user_id=user_id, message="Рассылка отправлена!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+    else:
+        vk.messages.send(user_id=user_id, message="Некорректная команда для рассылки!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+
+
+# Функция для отправки изображений
+def send_image(user_id, image_url):
+    upload_url = vk.photos.getMessagesUploadServer()["upload_url"]
+    response = requests.post(upload_url, files={"photo": open(image_url, "rb")})
+    result = response.json()
+    photo_data = vk.photos.saveMessagesPhoto(photo=result["photo"], server=result["server"], hash=result["hash"])[0]
+    attachments = f"photo{photo_data['owner_id']}_{photo_data['id']}"
+    vk.messages.send(user_id=user_id, message="Привет! Я полезный ассистент. Чем могу помочь?", attachment=attachments, random_id=random.getrandbits(31), keyboard=create_keyboard())
+
+# Основной код бота
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        handle_message(event)
+        message = event.text.lower()
+        user_id = event.user_id
+        
+        # Проверка команды для клавиатуры
+        if message == "подписаться":
+            if user_id not in subscribers:
+                subscribers.append(user_id)
+                vk.messages.send(user_id=user_id, message="Вы подписались на рассылку!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+            else:
+                vk.messages.send(user_id=user_id, message="Вы уже подписаны на рассылку!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+        
+        elif message == "отписаться":
+            if user_id in subscribers:
+                subscribers.remove(user_id)
+                vk.messages.send(user_id=user_id, message="Вы отписались от рассылки!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+            else:
+                vk.messages.send(user_id=user_id, message="Вы не подписаны на рассылку!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+        
+        elif message == "начать":
+            introduction_image_url = "C:\photoshop\ENORS3ws1cs.jpg"  # Замените на URL нужной вам картинки
+            send_image(user_id, introduction_image_url)
+            
+        elif message == "chatgpt":
+            vk.messages.send(user_id=user_id, message="Какой вопрос у вас есть?", random_id=random.getrandbits(31), keyboard=create_keyboard())
+        
+        elif message == "midjourney":
+            vk.messages.send(user_id=user_id, message="Что вы хотите сгенерировать?", random_id=random.getrandbits(31), keyboard=create_keyboard())
+        
+        elif message == "написать админу":
+            vk.messages.send(user_id=user_id, message="Напишите ваше сообщение администратору.", random_id=random.getrandbits(31))
+        
+        elif message.startswith("рассылка:") and user_id == ADMIN_USER_ID:
+            handle_newsletter(message)
+        
+        elif user_id in subscribers:
+            # Отправка запроса к ChatGPT
+            if message.startswith("chatgpt:"):
+                question = message.replace("chatgpt:", "").strip()
+                chatgpt_response = chat_gpt_request(question)
+                vk.messages.send(user_id=user_id, message=chatgpt_response, random_id=random.getrandbits(31), keyboard=create_keyboard())
+            
+            # Отправка запроса к Midjourney
+            elif message.startswith("midjourney:"):
+                request = message.replace("midjourney:", "").strip()
+                midjourney_response = midjourney_request(request)
+                vk.messages.send(user_id=user_id, message=midjourney_response, random_id=random.getrandbits(31), keyboard=create_keyboard())
+            
+            else:
+                # Отправка сообщения об ошибке, если пользователь ввел некорректную команду
+                vk.messages.send(user_id=user_id, message="Некорректная команда!", random_id=random.getrandbits(31), keyboard=create_keyboard())
+        
+        else:
+            # Отправка сообщения об ошибке, если пользователь не подписан на рассылку
+            vk.messages.send(user_id=user_id, message="Вы не подписаны на рассылку!", random_id=random.getrandbits(31), keyboard=create_keyboard())
