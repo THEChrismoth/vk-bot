43ce501e7a1628394a98f3ddb12ad65dca312f8f
THEChrismoth <30945154+THEChrismoth@users.noreply.github.com>
2024-10-17 12:21:50 +0300

    new func
    new command

--- handlers/comand.py
+++ handlers/comand.py
@@ -1,13 +1,40 @@
-from config import labeler
-from vkbottle.tools import PhotoMessageUploader
+from config import labeler, admin_id
 from reader import read_file
+from main import bot
 
+from vkbottle.tools import PhotoMessageUploader
+from vkbottle.bot import Message
 
 photo_uploader = PhotoMessageUploader(bot.api)
 
 
 @labeler.message(text="начать")
-async def start(message):
+async def start(message: Message):
     photo = await photo_uploader.upload("../images/wolf.png")
     doc = await read_file("../Docs/start.txt")
     await message.answer(doc, attachment=photo)
+
+
+@labeler.message(text="команды")
+async def start(message: Message):
+    doc = await read_file("../Docs/comand.txt")
+    await message.answer(doc)
+
+
+@labeler.message(text="промокоды")
+async def start(message: Message):
+    doc = await read_file("../Docs/promo.txt")
+    await message.answer(doc)
+
+
+@labeler.message(text="полезные ссылки")
+async def start(message: Message):
+    doc = await read_file("../Docs/resources.txt")
+    await message.answer(doc)
+
+
+@labeler.message(text="переслать")
+async def start(message: Message):
+    users_info = await bot.api.users.get(message.from_id)
+    message = f"Сообщение от подписчика {users_info[0].first_name}: {Message.text[9:]}"
+    await bot.api.messages.send(peer_id=admin_id, message=message, random_id=0)
