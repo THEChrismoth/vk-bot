e152c43dba6b574025c987e809f0538796baa04e
THEChrismoth <30945154+THEChrismoth@users.noreply.github.com>
2024-10-19 12:19:03 +0300

    Update state_comand.py

--- handlers/state_comand.py
+++ handlers/state_comand.py
@@ -2,12 +2,8 @@ from config import labeler, admin_id, GigaChat_authorization
 from bot import bot
 from vkbottle import BaseStateGroup, CtxStorage
 
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import HumanMessage
 from langchain.chat_models.gigachat import GigaChat
-
-import aiohttp
-import asyncio
-from aiohttp import ClientSession
 
 
 # Определяем состояния
@@ -18,21 +14,17 @@ class States(BaseStateGroup):
 
 ctx_storage = CtxStorage()
 
+# Авторизация в сервисе
+chat = GigaChat(
+    credentials=GigaChat_authorization,
+    verify_ssl_certs=False,
+)
 
-async def generate_response(message_text):
-    async with ClientSession() as session:
 
-        async def get_response():
-            async with session.post(
-                "https://api.gigachat.ai/v2/chats",
-                json={"credentials": GigaChat_authorization, "verify_ssl_certs": False},
-                data={"messages": [{"content": message_text}]},
-            ) as resp:
-                if resp.status == 200:
-                    return await resp.json()
-
-        response = await get_response()
-        return response["content"]
+async def generate_response(chat, message_text):
+    messages = [HumanMessage(content=message_text)]
+    response = chat(messages)
+    return response.content
 
 
 @labeler.message(text="Переслать")
@@ -65,7 +57,7 @@ async def start_forwarding(message):
 async def message_to_forward(message):
     # Получаем текст сообщения
     remaining_text = message.text
-    answer = await generate_response(message.text)
+    answer = await generate_response(chat, remaining_text)
 
     await message.answer(answer)
 
